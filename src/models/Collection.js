5bd34a535638b822150e939ad15883ec3468c019
jeFFF @ Tisseur de toile <jft@tisseur-de-toile.fr>
2023-12-18 15:32:19 +0100

    Fix error messages

--- src/models/Collection.js
+++ src/models/Collection.js
@@ -1,18 +1,18 @@
-import Model from './Model.js'
+import ModelClass from './Model.js'
 
 export default class collection {
     static make(Model = null, data = null) {
         if (Model === null) {
-            throw new Error('DATA missing to create collection ! A Model should be provided')
+            throw new Error('[COLLECTION] - DATA missing to create collection ! A Model should be provided')
         }
         if (Object.getPrototypeOf(Model).name !== ModelClass.name) {
-            throw new Error('The provided Object is not a Model definition')
+            throw new Error('[COLLECTION] - The provided Object is not a Model definition')
         }
         if (data === null) {
-            throw new Error('DATA missing to create collection ! An array of objects is required')
+            throw new Error('[COLLECTION] - DATA missing to create collection ! An array of objects is required')
         }
         if (Array.isArray(data) === false) {
-            throw new Error('DATA should be an Array !')
+            throw new Error('[COLLECTION] - DATA should be an Array !')
         }
         // https://stackoverflow.com/questions/4215737/how-to-convert-an-array-into-an-object
         return data
